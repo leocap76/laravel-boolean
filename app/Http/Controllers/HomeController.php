98f1df2ba121324c7e566c724db58864b819db01
Leonardo Capogna <leonardocapogna@gmail.com>
2021-02-09 17:54:39 +0100

    db founded

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Car;
 
 class HomeController extends Controller
 {
    public function index(){
+       // select * from Cars;
+       $Cars = Car::all();
+       dd($Cars);
        return view ('home');
    } //
 }
